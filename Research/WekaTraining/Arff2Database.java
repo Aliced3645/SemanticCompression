ffcd2cf06d73eb7eea881c4a9c7a29f04d088528
mrzhang <minrui_zhang@brown.edu>
2014-02-15 14:18:15 -0500

    Added a tool to transfer arff file to mysql database; added cps.arff

---
+++ Research/WekaTraining/Arff2Database.java
@@ -0,0 +1,28 @@
+package WekaTraining;
+
+import weka.core.*;
+import weka.core.converters.*;
+import java.io.*;
+
+  public class Arff2Database {
+ 
+    /**
+     * loads a dataset into a MySQL database
+     *
+     * @param args    the commandline arguments
+     */
+    public static void main(String[] args) throws Exception {
+      Instances data = new Instances(new BufferedReader(new FileReader(args[0])));
+      data.setClassIndex(data.numAttributes() - 1);
+ 
+      DatabaseSaver save = new DatabaseSaver();
+      save.setUrl("jdbc:mysql://localhost/testdb?"
+				+ "user=shu&password=shu");
+
+      save.setInstances(data);
+      save.setRelationForTableName(false);
+      save.setTableName(args[0]);
+      save.connectToDatabase();
+      save.writeBatch();
+    }
+  }
