43f9e53442d17ba84d30750bc8ce63ff5990c558
aliced3645 <aliced3645@gmail.com>
2014-03-13 11:23:51 -0500

    adding common IO lib

--- Research/Experiments/OptimizerTimeExperiment.java
+++ Research/Experiments/OptimizerTimeExperiment.java
@@ -1,15 +1,19 @@
 package Experiments;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import org.apache.commons.io.FileUtils;
 
 import Online.Optimizer;
 
 public class OptimizerTimeExperiment {
 
 	static String query1 = "SELECT GEREG, GESTCEN FROM cps;";
+	static String columnDir = "columns";
 	
 	static long measureOptimizerTime(Optimizer optimizer, String query) throws Exception{
 		long startTime = System.nanoTime();    
@@ -29,10 +33,13 @@ public class OptimizerTimeExperiment {
 		Connection connection = DriverManager
 				.getConnection("jdbc:mysql://localhost/metadata?"
 						+ "user=shu&password=shu");
-		Optimizer optimizer = new Optimizer(connection, "columns");
+		FileUtils.deleteDirectory(new File(columnDir));
+		
+		/*
+		Optimizer optimizer = new Optimizer(connection, columnDir);
 		long time1 = measureOptimizerTime(optimizer, query1);
 		long time2 = measureNormalSQLTime(connection, query1);
-		
+		*/
 		return;
 	}
 }
