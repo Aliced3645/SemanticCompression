b3d703d88bbd246ab9d6f364b761589d6eb02ff6
mrzhang <minrui_zhang@brown.edu>
2014-03-17 15:18:30 -0400

    Store predict column with columns it depends.

--- Research/Online/DecompressByDependency.java
+++ Research/Online/DecompressByDependency.java
@@ -34,8 +34,7 @@ public class DecompressByDependency {
 	InstancesHeader header;
 	
 	BufferedReader inputStream;
-	
-	static boolean MEASURE_TIME = true;
+
 	
 	public DecompressByDependency() throws SQLException {
 
@@ -299,7 +298,6 @@ public class DecompressByDependency {
 		resultInstances = new Instances(resultAttribute.name(), resultAttrinfo,
 				instances[0].size());
 
-		long time = System.nanoTime();
 		
 		for (int i = 0; i < instances[0].size(); i++) {
 
@@ -365,24 +363,192 @@ public class DecompressByDependency {
 		saver.setDestination(new File(sb.toString()));
 		saver.writeBatch();
 
-		//ends
-		if(MEASURE_TIME){
-			time = System.nanoTime() - time;
-			System.out.println("This prediction takes time: " + time);
-		}
 		
 		System.out
 				.println("Decompression done! The predicted column is saved at '"
 						+ predictFilesFolder + "' folder.");
 	}
 
+	public void decompress(String tableName, String columnName, List<String> dependencies,
+			String columnsFileFolder, String predictFilesFolder, String modelName)
+			throws Exception {
+		
+		readHeader(tableName);
+		
+		Classifier classifier = null;
+
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select model from "
+				+ tableName + " where attribute =  '" + columnName + "';");
+		if (resultSet.first()) {
+			InputStream in = resultSet.getBinaryStream(1);
+			File file = new File("Temp");
+			FileOutputStream fos = new FileOutputStream(file);
+			byte[] data = new byte[4096];
+			int count = -1;
+			while ((count = in.read(data, 0, 4096)) != -1) {
+				fos.write(data, 0, count);
+			}
+			fos.close();
+			classifier = (Classifier) SerializeUtils.readFromFile(file);
+		} else {
+			System.out.println("SQL exception: No such table/colunmn.");
+			return;
+		}
+
+		Instance predInstance = null;
+		Instances resultInstances = null;
+		Instances[] dependResultInstances = new Instances[dependencies.size()];
+
+
+
+		String[] dependArray = new String[dependencies.size()];
+		dependArray = dependencies.toArray(dependArray);
+		
+		for(String s : dependArray) System.out.println(s);
+
+		if (dependArray[0].equals("null")) {
+			System.out.println("Cannot predict this column: " + columnName);
+			return;
+		}
+
+		int[] dependIndex = new int[dependArray.length];
+		for (int i = 0; i < dependArray.length; i++) {
+			dependIndex[i] = header.attribute(dependArray[i]).index();
+		}
+
+		BufferedReader[] reader = new BufferedReader[dependArray.length];
+		for (int i = 0; i < reader.length; i++) {
+			reader[i] = new BufferedReader(new FileReader(columnsFileFolder
+					+ "/" + dependArray[i] + ".arff"));
+		}
+
+		Instances[] instances = new Instances[dependArray.length];
+		for (int i = 0; i < reader.length; i++) {
+			instances[i] = new Instances(reader[i]);
+		}
+
+		Attribute resultAttribute = header.attribute(columnName);
+		ArrayList<Attribute> resultAttrinfo = new ArrayList<Attribute>();
+		resultAttrinfo.add(resultAttribute);
+		resultInstances = new Instances(resultAttribute.name(), resultAttrinfo,
+				instances[0].size());
+		
+		Attribute[] dependResult = new Attribute[dependArray.length];
+		for(int i = 0; i < dependResult.length; i++) {
+			Attribute dependResultAttribute = header.attribute(dependArray[i]);
+			ArrayList<Attribute> dependResultinfo = new ArrayList<Attribute>();
+			dependResultinfo.add(dependResultAttribute);
+			dependResultInstances[i] = new Instances(dependResultAttribute.name(), dependResultinfo,
+					instances[0].size());
+		}
+
+		
+		for (int i = 0; i < instances[0].size(); i++) {
+
+			predInstance = new DenseInstance(header.numAttributes());
+			predInstance.setDataset(header);
+			
+			
+			for (int j = 0; j < dependArray.length; j++) {
+				Instance dependInstanceToAdd = new DenseInstance(1);
+				dependInstanceToAdd.setDataset(dependResultInstances[j]);
+				
+				if (header.attribute(dependIndex[j]).isNumeric()) {
+					double val;
+					try {
+						val = instances[j].get(i).value(0);
+					} catch (NumberFormatException e) {
+						e.printStackTrace();
+						break;
+					}
+					predInstance.setValue(dependIndex[j], val);
+					
+					dependInstanceToAdd.setValue(0, val);
+
+				} else {
+					String val = instances[j].get(i).stringValue(0);
+					predInstance.setValue(dependIndex[j], val);
+					
+					
+					dependInstanceToAdd.setValue(0, val);
+				}
+				
+				dependResultInstances[j].add(dependInstanceToAdd);
+
+			}
+			
+			CustomWEKAClassifier classifier2 = (CustomWEKAClassifier) classifier;
+
+			Instance instanceToAdd = new DenseInstance(1);
+
+			instanceToAdd.setDataset(resultInstances);
+
+			if (modelName.equals("M5P")) {
+
+				M5P m5p = (M5P) (classifier2.getWEKAClassifier());
+
+				if (header.attribute(columnName).isNumeric()) {
+
+					instanceToAdd.setValue(0,
+							m5p.classifyInstance(predInstance));
+				} else {
+					System.out.println("M5P can only predict numeric value.");
+					return;
+				}
+
+			}
+
+			else if (modelName.equals("REPTree")) {
+				if (header.attribute(columnName).isNumeric()) {
+					instanceToAdd.setValue(0,
+							Utilities.numericValue(classifier, predInstance));
+				} else {
+					instanceToAdd.setValue(0,
+							Utilities.nominalValue(classifier, predInstance));
+				}
+			}
+
+			resultInstances.add(instanceToAdd);
+		}
+
+		StringBuilder sb = new StringBuilder(predictFilesFolder);
+		sb.append('/');
+		sb.append(columnName);
+		sb.append(".arff");
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(resultInstances);
+		saver.setFile(new File(sb.toString()));
+		saver.setDestination(new File(sb.toString()));
+		saver.writeBatch();
+		
+		for(int i = 0; i < dependArray.length; i++) {
+			StringBuilder dsb = new StringBuilder(predictFilesFolder);
+			dsb.append('/');
+			dsb.append(dependArray[i]);
+			dsb.append(".arff");
+			ArffSaver dsaver = new ArffSaver();
+			dsaver.setInstances(dependResultInstances[i]);
+			dsaver.setFile(new File(dsb.toString()));
+			dsaver.setDestination(new File(dsb.toString()));
+			dsaver.writeBatch();
+		}
+
+		
+		System.out
+				.println("Decompression done! The predicted column is saved at '"
+						+ predictFilesFolder + "' folder.");
+	}
+	
 	public static void main(String[] args) throws Exception {
 		String tableName = args[0];
 		String columnName = args[1];
 		String columnFilesFolder = args[2];
 		String predictFilesFolder = args[3];
 		DecompressByDependency dbd = new DecompressByDependency();
-		dbd.decompress(tableName, columnName, columnFilesFolder, predictFilesFolder, "REPTree");
+		
+		dbd.decompress(tableName, columnName, dbd.getDependencies(tableName, columnName), columnFilesFolder, predictFilesFolder, "REPTree");
+		//dbd.decompress(tableName, columnName, columnFilesFolder, predictFilesFolder, "REPTree");
 		//HashMap<String, Object> map = new HashMap<String, Object>();
 		//map.put("PRERELG", 0.0);
 		//map.put("PTERN", -0.01);
